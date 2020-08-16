6096bcaa19fb3d05102c84d64b5e3192a4e410ca
SSDesth <carlosmontt19@hotmail.com>
2020-08-16 00:51:48 -0600

    Updates

---
+++ precache-manifest.3b9ce2925eacb588cb1edcefb722b905.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16fc9c7e5109667c6cdf7dc15cbc05ec",
+    "url": "/ReactRobotfriends/index.html"
+  },
+  {
+    "revision": "da5f820473674468c619",
+    "url": "/ReactRobotfriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "5c6fbc6f8ab6bb6a5941",
+    "url": "/ReactRobotfriends/static/css/main.0c99deb8.chunk.css"
+  },
+  {
+    "revision": "da5f820473674468c619",
+    "url": "/ReactRobotfriends/static/js/2.f62c00ff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactRobotfriends/static/js/2.f62c00ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c6fbc6f8ab6bb6a5941",
+    "url": "/ReactRobotfriends/static/js/main.d30f5f9e.chunk.js"
+  },
+  {
+    "revision": "ba1cb38be060207dff4b",
+    "url": "/ReactRobotfriends/static/js/runtime-main.539f5887.js"
+  },
+  {
+    "revision": "82e09f7862a1e26fb7dfe1d0f8adc87f",
+    "url": "/ReactRobotfriends/static/media/SEGA.82e09f78.TTF"
+  }
+]);
